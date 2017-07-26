685766e7cd9b9bab2c30db59a41e3922a641c672
mangalvidhu <mangal.vidhu@gmail.com>
2017-07-26 17:49:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
         speech: bodyB,
-        displayText: obj.result,
+        displayText: obj,
         source: 'crm-google-assistant-sample'
     });
 });
