7096b8ea816fd3334751d0bdad5af2ee577a263a
mangalvidhu <mangal.vidhu@gmail.com>
2017-07-26 11:43:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/google-assistant-for-crm', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.CRM-Activities ? req.body.result.parameters.CRM-Activities : "Seems like some problem. Speak again."
     return res.json({
         speech: speech,
         displayText: speech,
