689fe657b74641c2301c264974089fa650db2943
mangalvidhu <mangal.vidhu@gmail.com>
2017-07-26 17:26:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/version', (req, res) => {
     res.status(200).send("APIAI Webhook Integration. Version 1.0");
@@ -15,18 +15,18 @@ app.get('/', (req, res) => {
 });
 
 /* Handling all messenges */
-app.post('/crm-google-assistant', (req, res) => {
+app.post('/crm-google-assistant', function (req, res) {
     console.log(req.body);
     //console.log(req.body.result.parameters["CRMActivities"]);
     //Persist this in some database
     //Send out an email that new feedback has come in
-    res.status(200).json({
+    return res.status(200).json({
           speech: "Thank you for the feedback",
           displayText: "Thank you for the feedback",
           source: 'Hotel Feedback System'});
 });
 
-restService.post('/echo', function(req, res) {
+app.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
         speech: speech,
